ab9632a13b0ce2c729febb0101fef0f0fc4063b2
diwalaku <diwalaku@student.codam.nl>
2025-12-19 15:12:14 +0100

    Add ConfigParser class for parsing configuration files

---
+++ includes/ConfigParser.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "Config.hpp"
+#include <string>
+#include <vector>
+
+// The ConfigParser class will be responsible for parsing configuration files:
+// - Open and read the configuration file
+// - Parse the content line by line
+// - Store the parsed data into ServerConfig and LocationConfig structures
+// - Handle syntax errors and invalid configurations
+
+// Parses configuration files and populates ServerConfig structures
+class ConfigParser {
+	private:
+
+
+};
