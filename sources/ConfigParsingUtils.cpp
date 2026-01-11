963b0cd2053e75ff3404a3364a44a822f2d9ed70
Didi Walakutty <didiswalakutty@gmail.com>
2026-01-11 18:27:08 +0100

    Refine comment removal logic to handle inline comments more accurately

--- sources/ConfigParsingUtils.cpp
+++ sources/ConfigParsingUtils.cpp
@@ -36,17 +36,32 @@ bool ConfigParser::isEmptyFile(const std::string& file) const
 	return false;
 }
 
-// Removes comments from a line (anything after '#' and/or '//')
+// Removes all # comments, and // comments only if they appear after a semicolon.
 void ConfigParser::removeComments(std::string& line)
 {
+	// Init to npos, will hold the earliest comment position found
+	size_t commentPos = std::string::npos;
+
 	size_t hashPos = line.find('#');
 	if (hashPos != std::string::npos)
-		line.erase(hashPos);
-	
-	size_t slashPos = line.find("//");
-	if (slashPos != std::string::npos)
-		line.erase(slashPos);
+		commentPos = hashPos;
+
+	// finds '//' if after semicolon and updates commentPos
+	size_t semicolonPos = line.find(';');
+	if (semicolonPos != std::string::npos)
+	{
+		size_t slashPos = line.find("//", semicolonPos + 1);
+		if (slashPos != std::string::npos)
+		{
+			if (commentPos == std::string::npos || slashPos < commentPos)
+				commentPos = slashPos;
+		}
+	}
+
+	if (commentPos != std::string::npos)
+		line.erase(commentPos);
 }
+
 
 // Finds first and last non-whitespace characters and trims the line accordingly
 void ConfigParser::trimWhitespace(std::string& line)
@@ -69,16 +84,16 @@ bool ConfigParser::isLineEmpty(const std::string& line) const
 }
 
 // Splits a line by whitespace into tokens -> parseServerBlock, parseLocationBlock
-std::vector<std::string> ConfigParser::splitByWhitespace(const std::string& line) const
-{
-	// Implementation goes here
-}
+// std::vector<std::string> ConfigParser::splitByWhitespace(const std::string& line) const
+// {
+// 	// Implementation goes here
+// }
 
-// Splits a line by semicolon into tokens  -> parseServerBlock, parseLocationBlock
-std::vector<std::string> ConfigParser::splitBySemicolon(const std::string& line) const
-{
-	// Implementation goes here
-}
+// // Splits a line by semicolon into tokens  -> parseServerBlock, parseLocationBlock
+// std::vector<std::string> ConfigParser::splitBySemicolon(const std::string& line) const
+// {
+// 	// Implementation goes here
+// }
 
 
 // printParsedConfig
