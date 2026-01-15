511675f71a188a6fe9755081a1ea046ae450e6ac
Didi Walakutty <didiswalakutty@gmail.com>
2026-01-15 23:17:00 +0100

    Testing purposes

--- sources/configmaintester.cpp
+++ sources/configmaintester.cpp
@@ -1,143 +1,120 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <sstream>
 #include "../includes/Config.hpp"
-#include "../includes/ConfigParser.hpp" // whatever contains your ConfigParser class
+#include "../includes/ConfigParser.hpp"
 
-std::vector<std::string> ConfigParser::splitByWhitespace(const std::string& str) const
+std::string method_to_string(HTTPMethod m)
 {
-    std::vector<std::string> tokens;
-    std::string token;
-    std::istringstream iss(str);
-    while (iss >> token)
-        tokens.push_back(token);
-    return tokens;
-}
-
-bool ConfigParser::validateServerConfig(const ServerConfig& server) const
-{
-    // Minimal validation: host, port, root, index
-    if (server.host.empty() || server.port == 0 || server.root.empty() || server.index.empty())
-        return false;
-
-    // Validate locations
-    for (const auto& loc : server.locations)
+    switch (m)
     {
-        if (loc.path.empty())
-            return false;
+        case HTTPMethod::GET: return "GET";
+        case HTTPMethod::POST: return "POST";
+        case HTTPMethod::DELETE: return "DELETE";
+        default: return "UNKNOWN";
     }
-
-    return true;
 }
 
-
-std::optional<HTTPMethod> ConfigParser::stringToHTTPMethod(const std::string& str) const
-{
-    if (str == "GET") return HTTPMethod::GET;
-    if (str == "POST") return HTTPMethod::POST;
-    if (str == "DELETE") return HTTPMethod::DELETE;
-    return std::nullopt; // invalid method
-}
-
-
-void printHTTPMethods(const std::vector<HTTPMethod>& methods)
+void print_methods(const std::vector<HTTPMethod>& methods)
 {
     for (size_t i = 0; i < methods.size(); ++i)
     {
-        switch (methods[i])
-        {
-            case HTTPMethod::GET: std::cout << "GET"; break;
-            case HTTPMethod::POST: std::cout << "POST"; break;
-            case HTTPMethod::DELETE: std::cout << "DELETE"; break;
-        }
-        if (i != methods.size() - 1)
+        std::cout << method_to_string(methods[i]);
+        if (i + 1 < methods.size())
             std::cout << ", ";
     }
 }
 
-void printLocation(const LocationConfig& loc)
+void print_location(const LocationConfig& loc)
 {
-    std::cout << "  Location Path: " << loc.path << "\n";
-    std::cout << "    Root: " << loc.root << "\n";
-    std::cout << "    Index: " << loc.index << "\n";
-    std::cout << "    Autoindex: " << (loc.autoIndex ? "on" : "off") << "\n";
-    std::cout << "    Allowed Methods: ";
-    printHTTPMethods(loc.allowedMethods);
+    std::cout << "  Location:\n";
+    std::cout << "    path: " << loc.path << "\n";
+    std::cout << "    root: " << loc.root << "\n";
+    std::cout << "    index: " << loc.index << "\n";
+
+    std::cout << "    autoindex: ";
+    if (loc.autoIndex.has_value())
+        std::cout << (*loc.autoIndex ? "on" : "off") << "\n";
+    else
+        std::cout << "n/a\n";
+
+    std::cout << "    is_cgi: ";
+    if (loc.is_cgi.has_value())
+        std::cout << (*loc.is_cgi ? "true" : "false") << "\n";
+    else
+        std::cout << "n/a\n";
+
+    std::cout << "    uploadEnabled: ";
+    if (loc.uploadEnabled.has_value())
+        std::cout << (*loc.uploadEnabled ? "true" : "false") << "\n";
+    else
+        std::cout << "n/a\n";
+
+    std::cout << "    allowed methods: ";
+    print_methods(loc.allowedMethods);
     std::cout << "\n";
+
     if (loc.redirect.statusCode != 0)
-        std::cout << "    Redirect: " << loc.redirect.statusCode << " -> " << loc.redirect.targetURL << "\n";
-    std::cout << "\n";
+    {
+        std::cout << "    return/redirect:\n";
+        std::cout << "      status: " << loc.redirect.statusCode << "\n";
+        std::cout << "      target: " << loc.redirect.targetURL << "\n";
+    }
 }
 
-void printServer(const ServerConfig& server)
+
+void print_server(const ServerConfig& server)
 {
-    std::cout << "Server Name: " << server.serverName << "\n";
-    std::cout << "Host: " << server.host << "\n";
-    std::cout << "Port: " << server.port << "\n";
-    std::cout << "Root: " << server.root << "\n";
-    std::cout << "Index: " << server.index << "\n";
-    std::cout << "Max Body Size: " << server.maxBodySize << "\n";
-
-    std::cout << "Allowed Methods: ";
-    printHTTPMethods(server.allowedMethods);
+    std::cout << "=================================\n";
+    std::cout << "ServerConfig\n";
+    std::cout << "---------------------------------\n";
+    std::cout << "server_name: " << server.serverName << "\n";
+    std::cout << "host: " << server.host << "\n";
+    std::cout << "port: " << server.port << "\n";
+    std::cout << "root: " << server.root << "\n";
+    std::cout << "index: " << server.index << "\n";
+    std::cout << "allowed methods: ";
+    print_methods(server.allowedMethods);
     std::cout << "\n";
+	std::cout << "autoindex: " << (server.autoIndex ? "on" : "off") << "\n";
+    std::cout << "max_body_size: " << server.maxBodySize << "\n";
 
-    std::cout << "Error Pages:\n";
-    for (std::map<int,std::string>::const_iterator it = server.errorPages.begin(); it != server.errorPages.end(); ++it)
+
+    if (!server.errorPages.empty())
     {
-        std::cout << "  " << it->first << " -> " << it->second << "\n";
+        std::cout << "error pages:\n";
+        for (std::map<int, std::string>::const_iterator it = server.errorPages.begin();
+             it != server.errorPages.end(); ++it)
+        {
+            std::cout << "  " << it->first << " -> " << it->second << "\n";
+        }
     }
 
-    std::cout << "\nLocations:\n";
+    std::cout << "\nlocations:\n";
     for (size_t i = 0; i < server.locations.size(); ++i)
     {
-        printLocation(server.locations[i]);
+        print_location(server.locations[i]);
     }
-    std::cout << "-------------------------------------\n";
+    std::cout << "=================================\n\n";
 }
 
+// Tester for Parsing Config File
 int main()
 {
     ConfigParser parser;
-    std::string file = "../config/default.conf"; // your config file
+    std::string filepath = "../config/default.conf";
 
-    if (!parser.parseConfigFile(file))
-    {
-        std::cerr << "Failed to parse config file.\n";
-        return 1;
-    }
-
-    std::vector<ServerConfig> servers = parser.getServers(); // or _servers depending on your class
-    for (size_t i = 0; i < servers.size(); ++i)
-    {
-        printServer(servers[i]);
-    }
+    if (parser.parseConfigFile(filepath))
+	{
+		const std::vector<ServerConfig>& servers = parser.getServers();
+		for (size_t i = 0; i < servers.size(); ++i)
+			print_server(servers[i]);
+	}
+    else
+		std::cout << "error in parsing" << std::endl;
 
     return 0;
 }
-
-
-
-// #include "../includes/ConfigParser.hpp"
-// #include <iostream>
-
-// // g++ -std=c++17 -Wall -Wextra -Werror ConfigParser.cpp ConfigParsingUtils.cpp configmaintester.cpp -o configTest
-// // For testing if conf file, not empty, removing comments, whitespace and skip line if empty
-// int main()
-// {
-//     ConfigParser parser;
-
-//     std::string filepath = "../config/default.conf";
-
-//     if (!parser.parseConfigFile(filepath)) {
-//         std::cerr << "Failed to parse config file: " << filepath << std::endl;
-//         return 1;
-//     }
-
-//     std::cout << "Parse successful!" << std::endl;
-//     return 0;
-// }
 
 
 
@@ -161,14 +138,3 @@ int main()
 // // 	return 0;
 // // }
 
-// // Tester for ConfigParser
-// int	main()
-// {
-// 	ConfigParser file;
-// 	std::string input = "./config/default.conf";
-
-// 	if (!file.parseConfigFile(input))
-// 		return 1;
-// 	file.printParsedConfig();
-// 	return 0;
-// }
