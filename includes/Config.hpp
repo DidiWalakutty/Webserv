e397b246151420b76422a3b80ab6cc5487330307
Didi Walakutty <didiswalakutty@gmail.com>
2026-01-15 23:16:25 +0100

    Update on LocationConfig and ServerConfig to use std::optional for bools

--- includes/Config.hpp
+++ includes/Config.hpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <optional>
 
 enum class HTTPMethod
 {
@@ -25,12 +26,12 @@ struct LocationConfig
 	std::string path;   		/* URL path for this location "/", "/upload"*, "/images" */
 	std::string root;   		/* Filesystem/Root directory for this location to serve from "www/uploads"*/
 	std::string index;  		/* Index file (default index.html). Default file to serve if URL is a directory */
-
-	bool autoIndex = false; 	/* Enable or disable directory listing */
-	bool is_cgi = false;   		/* True if this location executes CGI scripts */
-	bool uploadEnabled = false; /* True if file uploads are allowed in this location */
-	
 	std::vector<HTTPMethod> allowedMethods; 	/* Allowed HTTP methods */
+
+	std::optional<bool> autoIndex; 	/* Enable or disable directory listing */
+	std::optional<bool> is_cgi;   		/* True if this location executes CGI scripts */
+	std::optional<bool> uploadEnabled; /* True if file uploads are allowed in this location */
+	
 	Redirect redirect;							/* Optional redirect for this location (status code + target URL)*/
 
 	bool method_allowed(HTTPMethod m) const;	/* Checks if a specific HTTP method is allowed */
@@ -43,11 +44,11 @@ struct ServerConfig
 	std::string serverName;         		/* Server name for virtual hosting */
 	std::string host;              			/* Server host (IP or domain)*/
 	std::string root;                		/* Root directory for the server */
+	std::string index;               		/* Index file for the server: filename */
+	std::vector<HTTPMethod> allowedMethods; /* Default allowed HTTP methods for the server */
 	int port;                       		/* Server port */
 
-	std::string index;               		/* Index file for the server: filename */
 	bool autoIndex = false;         		/* Enable or disable directory listing for the server */
-	std::vector<HTTPMethod> allowedMethods; /* Default allowed HTTP methods for the server */
 	size_t maxBodySize = 10485760; 			/* Max allowed body size in bytes for requests to this server: 10 mb. */
 
 	std::map<int, std::string> errorPages; 	/* Custom error pages mapped by HTTP status code */
