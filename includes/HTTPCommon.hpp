1efa14b145a582aceca63eb51f843b92d7b2a53d
diwalaku <diwalaku@student.codam.nl>
2026-01-26 16:26:04 +0100

    updated the functions

--- includes/HTTPCommon.hpp
+++ includes/HTTPCommon.hpp
@@ -76,7 +76,7 @@ enum HTTPState
  * @brief Struct to hold HTTP message details.
  *
  */
-struct HTTPMesage
+struct HTTPMessage
 {
 	std::string code;
 	std::string message;
@@ -123,7 +123,7 @@ enum class HTTPProtocolVersion
 class HTTPCommon
 {
 public:
-	static const std::unordered_map<HTTPState, HTTPMesage> HTTPStatusMap;
+	static const std::unordered_map<HTTPState, HTTPMessage> HTTPStatusMap;
 
 	static HTTPMethod stringToMethod(const std::string method);
 	static HTTPProtocolVersion stringToProtocolVersion(const std::string version);
