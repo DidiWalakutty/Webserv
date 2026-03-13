27d0f63a3923a714a6a2d1279f80c257b125fed6
diwalaku <diwalaku@student.codam.nl>
2026-03-13 21:32:27 +0100

    Added cgi_extension key=value

--- includes/Config.hpp
+++ includes/Config.hpp
@@ -24,9 +24,11 @@ struct LocationParse
 	bool autoIndex = false; 	/* Enable or disable directory listing */
 	bool is_cgi = false;   		/* True if this location executes CGI scripts */
 	bool uploadEnabled = false; /* True if file uploads are allowed in this location */
-	std::string cgi_executable; /* CGI executable name to call for CGI requests */
-	size_t maxBodySize = 0;		/* Max allowed body size in bytes for this location */
 	
+	std::string cgi_extension; 	/* If is_cgi is true, the file extension that triggers CGI execution (e.g., .py) */
+	std::string cgi_executable; /* needed???CGI executable name to call for CGI requests */
+	
+	size_t maxBodySize = 0;		/* Max allowed body size in bytes for this location */
 	Redirect redirect;							/* Optional redirect for this location (status code + target URL)*/
 
 	bool method_allowed(HTTPMethod m) const;	/* Checks if a specific HTTP method is allowed */
