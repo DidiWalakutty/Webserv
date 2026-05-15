012e17d787267907b3b3a8076653222d482bacec
goksu <akaya-oz@student.codam.nl>
2026-05-15 18:59:57 +0200

    cgi webpage post implementation form

--- www/cgi-bin/test.py
+++ www/cgi-bin/test.py
@@ -1,13 +1,33 @@
 #!/usr/bin/python3
+import os, sys
+
+method = os.environ.get("REQUEST_METHOD", "GET")
+query  = os.environ.get("QUERY_STRING", "")
+
+# For POST, read body from stdin; for GET, use QUERY_STRING
+if method == "POST":
+    length = int(os.environ.get("CONTENT_LENGTH", 0) or 0)
+    raw = sys.stdin.read(length) if length > 0 else ""
+else:
+    raw = query
+
+# Parse key=value&key=value
+params = {}
+for pair in raw.split("&"):
+    if "=" in pair:
+        k, v = pair.split("=", 1)
+        params[k] = v
+
+name = params.get("name", "stranger")
+age  = params.get("age", "")
+age_str = f", age {age}" if age else ""
 
 print("Content-Type: text/html")
 print()
-
-print("""
-<html>
+print(f"""<html>
 <head><title>Python CGI</title></head>
 <body>
-<h1>Python CGI works!</h1>
+<h1>Hello, {name}{age_str}!</h1>
 </body>
-</html>
-""")
+</html>""")
+
